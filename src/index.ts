f64a4d26e094f0311c33d36600a6432a10684cdb
Gerard Clos <clos.gerard@gmail.com>
2025-06-05 14:10:08 +0200

    chore: export missing type

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ export * from './compiler'
 export * from './parser'
 export * from './providers'
 
+export { type Fragment } from './parser/interfaces'
+
 export { default as CompileError } from './error/error'
