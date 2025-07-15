1ec12e2cd5848e1a4362ebe5d6d7b8387e675bc4
Gerard <clos.gerard@gmail.com>
2025-07-15 14:26:00 +0200

    feat: add tool result content type

--- src/providers/openai-responses/adapter.ts
+++ src/providers/openai-responses/adapter.ts
@@ -99,7 +99,7 @@ function fromPromptlContentToOpenAIResponseContent(
           file_data: toOpenAiFileData(item),
         })
         return acc
-      } else if (ContentType.toolCall) {
+      } else if (type === ContentType.toolCall) {
         acc.toolCalls.push({
           type: 'function_call',
           call_id: item.toolCallId,
@@ -154,67 +154,67 @@ type ToolMappings = {
 }
 const fromOpenAiResponseToPromptlMessage =
   (toolMappings: ToolMappings) =>
-    (message: MessageInputItem): PromptlMessage | null => {
-      if (isReasoning(message)) {
-        return parseReasoning(message)
-      }
-
-      if (isOutputMessage(message)) {
-        return parseOutputMessage({
-          message,
-          webSearch: toolMappings.websearch,
-          fileSearch: toolMappings.fileSearch,
-        })
-      }
-
-      if (isInputMessage(message)) {
-        return parseInputMessage(message)
-      }
-
-      if (isFunctionCall(message)) {
-        toolMappings.userTools.set(message.call_id, message.name)
-        return parseFunctionCall(message)
-      }
-
-      if (isFunctionCallOutput(message)) {
-        return parseFunctionCallOutput({
-          message,
-          toolNameMap: toolMappings.userTools,
-        })
-      }
-
-      if (isWebsearchCall(message)) {
-        return parseWebsearchCall({
-          message,
-          webSearch: toolMappings.websearch,
-        })
-      }
-
-      if (isFileSearchCall(message)) {
-        return parseFileSearch({
-          message,
-          fileSearch: toolMappings.fileSearch,
-        })
-      }
-
-      if (isSimpleInputMessage(message)) {
-        return parseSimpleInputMessage(message)
-      }
-
-      if (message.type === 'item_reference') return null
-
-      if (message.type === 'computer_call') {
-        throw new Error('Not implemented computer_call')
-      }
-
-      if (message.type === 'computer_call_output') {
-        throw new Error('Not implemented computer_call_output')
-      }
-
-      throw new Error(
-        `Unknown message type ${message.type} in OpenAIResponse adapter`,
-      )
+  (message: MessageInputItem): PromptlMessage | null => {
+    if (isReasoning(message)) {
+      return parseReasoning(message)
     }
+
+    if (isOutputMessage(message)) {
+      return parseOutputMessage({
+        message,
+        webSearch: toolMappings.websearch,
+        fileSearch: toolMappings.fileSearch,
+      })
+    }
+
+    if (isInputMessage(message)) {
+      return parseInputMessage(message)
+    }
+
+    if (isFunctionCall(message)) {
+      toolMappings.userTools.set(message.call_id, message.name)
+      return parseFunctionCall(message)
+    }
+
+    if (isFunctionCallOutput(message)) {
+      return parseFunctionCallOutput({
+        message,
+        toolNameMap: toolMappings.userTools,
+      })
+    }
+
+    if (isWebsearchCall(message)) {
+      return parseWebsearchCall({
+        message,
+        webSearch: toolMappings.websearch,
+      })
+    }
+
+    if (isFileSearchCall(message)) {
+      return parseFileSearch({
+        message,
+        fileSearch: toolMappings.fileSearch,
+      })
+    }
+
+    if (isSimpleInputMessage(message)) {
+      return parseSimpleInputMessage(message)
+    }
+
+    if (message.type === 'item_reference') return null
+
+    if (message.type === 'computer_call') {
+      throw new Error('Not implemented computer_call')
+    }
+
+    if (message.type === 'computer_call_output') {
+      throw new Error('Not implemented computer_call_output')
+    }
+
+    throw new Error(
+      `Unknown message type ${message.type} in OpenAIResponse adapter`,
+    )
+  }
 
 export const OpenAIResponsesAdapter: ProviderAdapter<MessageInputItem> = {
   type: 'openaiResponses',
