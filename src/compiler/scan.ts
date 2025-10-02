7ad016d68b9d6128968a3b131bf29dfaf057ea4d
andresgutgon <andresgutgon@gmail.com>
2025-10-02 11:19:10 +0200

    Revert "Upgrade to from Zod v3 to Zod v4 (#41)"
    This reverts commit a8bc5bca3669786592d05fd78561d2e61afbb673.

--- src/compiler/scan.ts
+++ src/compiler/scan.ts
@@ -319,8 +319,8 @@ export class Scan {
         this.configSchema?.parse(parsedObj)
       } catch (err) {
         if (isZodError(err)) {
-          err.issues.forEach((issue) => {
-            const { message, path } = getMostSpecificError(issue)
+          err.errors.forEach((error) => {
+            const { message, path } = getMostSpecificError(error)
 
             const range = findYAMLItemPosition(
               parsedYaml.contents as YAMLItem,
